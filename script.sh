f014a64583071c0f816b4be703e0dc6848f9edc0
valiantlynx <gormerykombo@gmail.com>
2022-10-28 19:23:24 +0200

    kinda had to restart from bootstrap mode

--- script.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/dfinity/ic
-npx ts-node modifyServiceworker.tsx
-pushd ic/typescript/service-worker
-npm install
-npm run build
-
-pushd
-
-cp -r ic/typescript/service-worker/dist-prod/ serviceworker/
